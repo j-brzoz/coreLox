0f8bd54bd9b87808232ba7b0252347b5a7276312
JanBrzozowski <83976408+j-brzoz@users.noreply.github.com>
2025-09-03 15:56:43 +0200

    feat: add disassembling chunks for debug

---
+++ debug.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+#include "debug.h"
+
+void disassembleChunk(Chunk* chunk, const char* name) {
+    printf("== %s ==\n", name);
+
+    for (int16_t offset = 0; offset < chunk->count;) {
+        // after disassembling the instruction at the given offset,
+        // it return the offset of the next instruction
+        offset = disassembleInstruction(chunk, offset);
+    }
+}
+
+static int16_t simpleInstruction(const char* name, int16_t offset) {
+    printf("%s\n", name);
+    return offset + 1;
+}
+
+int16_t disassembleInstruction(Chunk* chunk, int offset) {
+    printf("%04d ", offset);
+
+    uint8_t instruction = chunk->code[offset];
+    switch (instruction) {
+        case OP_RETURN:
+            return simpleInstruction("OP_RETURN", offset);
+        default:
+            printf("Unknown opcode %d\n", instruction);
+            return offset + 1;
+    }
+}
--- main.c
+++ main.c
@@ -1,12 +1,13 @@
 #include "common.h"
 #include "chunk.h"
+#include "debug.h"
 
 int main(int argc, const char* argv[]) {
     Chunk chunk;
     initChunk(&chunk);
     writeChunk(&chunk, OP_RETURN);
 
-    disassemblyChunk(&chunk, "test chunk");
+    disassembleChunk(&chunk, "test chunk");
     freeChunk(&chunk);
     return 0;
 }
