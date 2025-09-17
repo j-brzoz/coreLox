01cf8fe2b534d54bdae188318c5867b2ba0e08a2
JanBrzozowski <83976408+j-brzoz@users.noreply.github.com>
2025-09-17 02:30:30 +0200

    feat: add NaN-boxing optimization

--- value.c
+++ value.c
@@ -28,15 +28,34 @@ void writeValueArray(ValueArray* array, Value value) {
 }
 
 void printValue(Value value) {
+#ifdef NAN_BOXING
+    if (IS_BOOL(value)) {
+        printf(AS_BOOL(value) ? "true" : "false");
+    } else if (IS_NIL(value)) {
+        printf("nil");
+    } else if (IS_NUMBER(value)) {
+        printf("%g", AS_NUMBER(value));
+    } else if (IS_OBJECT(value)) {
+        printObject(value);
+    } 
+#else
     switch (value.type) {
         case VAL_BOOL:      printf(AS_BOOL(value) ? "true" : "false"); break;
         case VAL_NIL:       printf("nil"); break;
         case VAL_NUMBER:    printf("%g", AS_NUMBER(value)); break;
         case VAL_OBJECT:    printObject(value); break;
     }
+#endif
 }
 
 bool valuesEqual(Value a, Value b) {
+#ifdef NAN_BOXING
+    // for IEEE 754, this ensures NaN != NaN
+    if (IS_NUMBER(a) && IS_NUMBER(a)) {
+        return AS_NUMBER(a) == AS_NUMBER(b);
+    }
+    return a == b;
+#else
     if (a.type != b.type) return false;
 
     switch (a.type) {
@@ -46,4 +65,5 @@ bool valuesEqual(Value a, Value b) {
         case VAL_OBJECT:    return AS_OBJECT(a) == AS_OBJECT(b);
         default:            return false; // unreachable
     }
+#endif
 }
--- value.h
+++ value.h
@@ -7,6 +7,48 @@
 typedef struct Object Object;
 typedef struct ObjectString ObjectString;
 
+#ifdef NAN_BOXING
+#include <string.h>
+
+#define SIGN_BIT    ((uint64_t)0x8000000000000000)
+#define QNAN        ((uint64_t)0x7ffc000000000000) // quiet NaN
+
+#define TAG_NIL     1 // 01
+#define TAG_FALSE   2 // 10
+#define TAG_TRUE    3 // 11
+
+typedef uint64_t Value;
+
+#define IS_BOOL(value)      ((value | 1) == TRUE_VAL)
+#define IS_NIL(value)       ((value) == NIL_VAL)
+#define IS_NUMBER(value)    (((value) & QNAN) != QNAN)
+#define IS_OBJECT(value)    (((value) & (QNAN | SIGN_BIT)) == (QNAN | SIGN_BIT))
+
+#define AS_BOOL(value)      ((value) == TRUE_VAL)
+#define AS_NUMBER(value)    valueToNum(value)
+#define AS_OBJECT(value)    ((Object*)(uintptr_t)((value) & ~(SIGN_BIT | QNAN)))
+
+#define BOOL_VAL(b)         ((b) ? TRUE_VAL : FALSE_VAL)
+#define FALSE_VAL           ((Value)(uint64_t)(QNAN | TAG_FALSE))
+#define TRUE_VAL            ((Value)(uint64_t)(QNAN | TAG_TRUE))
+#define NIL_VAL             ((Value)(uint64_t)(QNAN | TAG_NIL))
+#define NUMBER_VAL(num)     numToValue(num)
+#define OBJECT_VAL(obj)     (Value)(SIGN_BIT | QNAN | (uint64_t)(uintptr_t)(obj))
+
+static inline double valueToNum(Value value) {
+    double num;
+    memcpy(&num, &value, sizeof(Value));
+    return num;
+}
+
+static inline Value numToValue(double num) {
+    Value value;
+    memcpy(&value, &num, sizeof(double));
+    return value;
+}
+
+#else
+
 typedef enum {
     VAL_BOOL,
     VAL_NIL,
@@ -36,6 +78,8 @@ typedef struct {
 #define NIL_VAL             ((Value){VAL_NIL,       {.number = 0}})
 #define NUMBER_VAL(value)   ((Value){VAL_NUMBER,    {.number = value}})
 #define OBJECT_VAL(obj)     ((Value){VAL_OBJECT,    {.object = (Object*)obj}})
+
+#endif
 
 // dynamic array storing data
 typedef struct {
